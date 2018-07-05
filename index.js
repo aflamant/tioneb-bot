68dfb1b20dc5e470257475ccc24322fd637a0838
Antoine Flamant <flamant.antoine@gmail.com>
2018-07-05 19:38:03 +0200

    Ajouté commande "say"

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+const config = require("./config.json");
 
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-bot.on('ready', function() {
-  bot.user.setActivity('ignorer les conversations').catch(console.error)
-  console.log('Bot connecté.')
+bot.on('ready', () => {
+  bot.user.setActivity(config.activity).catch(console.error);
+  console.log('Bot connecté.');
 })
 
 bot.on('guildMemberAdd', member => {
@@ -13,26 +13,47 @@ bot.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'general');
   // Do nothing if the channel wasn't found on this server
   if (!channel) {
-    console.log('erreur')
-    console.log(channel)
-    return
+    console.log('erreur');
+    console.log(channel);
+    return;
   }
   // Send the message, mentioning the member
   channel.send(`Eh salut ${member}, comment tu vas ?`);
 });
 
 bot.on('guildMemberAvailable', member => {
+
   if (Math.random() * 100 > 90) {
     const channel = member.guild.channels.find('name', 'general');
-    channel.send('Eh salut')
+    channel.send('Eh salut');
   }
 
 })
 
-bot.on('message', function (msg) {
-  if (msg.content === '!ping') {
-    msg.channel.send('pong!')
+bot.on('message', async message => {
+
+  if (message.author.bot) return;
+
+
+
+
+
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  if (command === 'ping') {
+    msg.channel.send('pong!');
+  }
+
+  if (command === 'say') {
+    const sayMessage = args.join(" ");
+
+    message.delete().catch(O_o=>{});
+
   }
 })
 
-bot.login(process.env.APP_TOKEN)
+bot.login(config.token)
