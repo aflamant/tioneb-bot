aa1a3bf748d4068c0e23d460434125251607e75c
Antoine Flamant <flamant.antoine@gmail.com>
2019-11-02 15:41:55 +0100

    Changement du channel d'annonces musiques

--- commands/music/play.js
+++ commands/music/play.js
@@ -116,7 +116,7 @@ function playSong(queue, message) {
           module.exports.dispatcher = dispatcher;
           module.exports.queue = queue;
           voiceChannel = queue[0].voiceChannel;
-          return message.guild.channels.find(channel => channel.name === 'general').send(`:musical_note:   Now playing: ${queue[0].title} - ${queue[0].duration}`);
+          return message.guild.channels.find(channel => channel.name === 'commandes').send(`:musical_note:   Now playing: ${queue[0].title} - ${queue[0].duration}`);
         })
         .on('finish', () => {
           console.log('Next song');
@@ -125,7 +125,7 @@ function playSong(queue, message) {
             return playSong(queue, message);
           } else {
             isPlaying = false;
-            message.guild.channels.find(channel => channel.name === 'general').send(`:musical_note:   Fin de la playlist atteinte`);
+            message.guild.channels.find(channel => channel.name === 'commandes').send(`:musical_note:   Fin de la playlist atteinte`);
             console.log('Reached the end of the playlist, leaving voice channel.');
             return voiceChannel.leave();
           }
